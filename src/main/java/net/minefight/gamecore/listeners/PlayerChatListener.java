45bcc076441fe98b2a5c3f42977d86d6dd9c538b
Daan <daanpl@live.nl>
2023-05-23 18:53:34 +0200

    fixed chat format

--- src/main/java/net/minefight/gamecore/listeners/PlayerChatListener.java
+++ src/main/java/net/minefight/gamecore/listeners/PlayerChatListener.java
@@ -9,6 +9,7 @@ import net.minefight.gamecore.utils.ChatUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import javax.swing.*;
 
@@ -25,11 +26,10 @@ public class PlayerChatListener implements Listener {
         event.renderer(((player, displayName, message, viewer) -> {
             User user = luckPerms.getUserManager().getUser(player.getUniqueId());
             CachedMetaData metaData = user.getCachedData().getMetaData();
-
             String tagcolor = metaData.getMetaValue("tagcolor");
             String prefix = metaData.getMetaValue("chatprefix");
-
-            return ChatUtils.color(prefix + " " + player.getName() + "<white>: ").append(message);
+            String rank = "<" + tagcolor + ">" + prefix + player.getName() + "</" + tagcolor + "><reset>";
+            return ChatUtils.color(rank + "<white>: ").append(message);
         }));
     }
 
